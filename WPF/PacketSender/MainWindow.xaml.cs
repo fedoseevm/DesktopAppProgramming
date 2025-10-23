e192b79e4a8de18f0b52d19875f244320ed45d10
fedoseevm <maksim.fedoseev122@gmail.com>
2025-10-23 14:21:46 +0200

    Add PacketSender

---
+++ WPF/PacketSender/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PacketSender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sendPacketsBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private bool IfIPCorrect()
+        {
+            Regex regex = new Regex(@"^$");
+        }
+    }
+}
