8b56f001b42c5d73035d00efe734c9696c1a9ba6
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-13 14:17:02 +0100

    Small changes

--- WPF/EntityFrameworkDB1/obj/Debug/net8.0-windows/EntityFrameworkDB1.AssemblyInfo.cs
+++ WPF/EntityFrameworkDB1/obj/Debug/net8.0-windows/EntityFrameworkDB1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityFrameworkDB1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557dd22418519428a46bad7b5307c61111333632")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityFrameworkDB1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityFrameworkDB1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
