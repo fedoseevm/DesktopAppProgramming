b612e3a84c610c68abbb00c41964edcdec1bb866
fedoseev <maksim.fedoseev122@gmail.com>
2025-11-20 14:17:07 +0100

    Add RadioButtons_Colors

---
+++ WPF/RadioButtons_Colors/KolorContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioButtons_Colors
+{
+    internal class KolorContext : DbContext
+    {
+        public DbSet<Kolor> Kolory { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=../../../kolor.db");
+        }
+    }
+}
---
+++ WPF/RadioButtons_Colors/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioButtons_Colors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KolorContext kolorContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+            kolorContext = new KolorContext();
+            kolorContext.Database.EnsureCreated();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Brush lastColor = label.Background;
+            ramka.BorderBrush = lastColor;
+
+            RadioButton RB = (RadioButton)sender;
+            //MessageBox.Show(RB.Tag.ToString());
+            Brush newColor = (Brush)new BrushConverter().ConvertFromString(RB.Content.ToString());
+            label.Background = newColor;
+
+            string kolor = RB.Content.ToString();
+            kolorContext.Kolory.Add(new Kolor { Nazwa = kolor });
+            lista.Items.Add(kolor);
+            kolorContext.SaveChanges();
+        }
+    }
+}
