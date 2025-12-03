a902191bb6f8317500a739ef0746ba18c559ba70
fedoseevm <maksim.fedoseev122@gmail.com>
2025-12-03 15:08:42 +0100

    Add ModelViewListItem

---
+++ WPF/ModelViewListItem/TodoItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViewListItem
+{
+    public partial class TodoItemViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string text;
+
+        [ObservableProperty]
+        private bool isDone;
+
+        public override string ToString()
+        {
+            return text;
+        }
+    }
+}
---
+++ WPF/ModelViewListItem/TodoListViewModel.cs
@@ -0,0 +1,60 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViewListItem
+{
+    public partial class TodoListViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string newTodoItem = string.Empty;
+
+        public ObservableCollection<TodoItemViewModel> TodoItems { get; } = new();
+
+        public TodoListViewModel()
+        {
+            TodoItems.Add(new TodoItemViewModel { Text = "Umyj się" });
+            TodoItems.Add(new TodoItemViewModel { Text = "Posprzątaj pokój" });
+        }
+
+        [RelayCommand]
+        private void AddNewItem()
+        {
+            if (NewTodoItem.Trim() != string.Empty)
+                TodoItems.Add(new TodoItemViewModel { Text = newTodoItem });
+            NewTodoItem = string.Empty;
+        }
+
+        [RelayCommand]
+        private void RemoveItem(TodoItemViewModel item)
+        {
+            TodoItems.Remove(item);
+        }
+
+        [RelayCommand]
+        private void SelectAll()
+        {
+            foreach(var item in TodoItems)
+            {
+                item.IsDone = true;
+            }
+        }
+
+        [RelayCommand]
+        private void RemoveSelected()
+        {
+            for (int i = 0; i < TodoItems.Count; i++)
+            {
+                if (TodoItems[i].IsDone)
+                {
+                    TodoItems.RemoveAt(i--);
+                }
+            }
+        }
+    }
+}
