c91bbbef49775ee4556d6f6e41b4f6926d72a244
fedoseevm <maksim.fedoseev122@gmail.com>
2025-12-04 13:35:30 +0100

    Add project

---
+++ WPF/PersonViewModel/PersonViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsobaViewModel
+{
+    public partial class PersonViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string firstName;
+
+        [ObservableProperty]
+        private string lastName;
+
+        [ObservableProperty]
+        private string fullName;
+
+
+        partial void OnFirstNameChanged(string value)
+        {
+            FullName = $"{FirstName} {LastName}";
+        }
+
+        partial void OnLastNameChanged(string value)
+        {
+            FullName = $"{FirstName} {LastName}";
+        }
+    }
+}
