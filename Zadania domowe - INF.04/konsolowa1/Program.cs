e54438f98c5d8b798479ddadaaeebb23fd6008cd
fedoseevm <maksim.fedoseev122@gmail.com>
2026-01-16 20:08:01 +0100

    Finish task

--- Zadania domowe - INF.04/konsolowa1/Program.cs
+++ Zadania domowe - INF.04/konsolowa1/Program.cs
@@ -17,9 +17,10 @@ namespace konsolowa1
                 int n;
                 if (int.TryParse(Console.ReadLine(), out n) && n >= 3 && n <= 20)
                 {
+                    Console.WriteLine();
                     for (int i = 0; i < n; i++)
                     {
-                        Console.Write($"Podaj liczbę {i}: ");
+                        Console.Write($"Podaj liczbę {i + 1}: ");
                         int liczba;
                         if (!int.TryParse(Console.ReadLine(), out liczba) || liczba < 1 || liczba > 9999)
                         {
@@ -29,17 +30,60 @@ namespace konsolowa1
                         else lista.Add(liczba);
                     }
 
+                    WyswietlTablice("Tablica przed sortowaniem:", lista);
+
                     List<int> sumyCyfr = new List<int>();
                     foreach (int liczba in lista)
                     {
-                        sumyCyfr.Add(GetDigitsSum(liczba));
+                        sumyCyfr.Add(ObliczSumeCyfr(liczba));
                     }
+
+                    WyswietlTablice("Tablica z sumami cyfr", lista, sumyCyfr);
+
+                    sumyCyfr = SortowanieBabelkowe(sumyCyfr);
+
+                    for (int i = 0; i < sumyCyfr.Count; i++)
+                    {
+                        for (int j = 0; j < lista.Count; j++)
+                        {
+                            if (sumyCyfr[i] == ObliczSumeCyfr(lista[j]))
+                            {
+                                int temp = lista[i];
+                                lista[i] = lista[j];
+                                lista[j] = temp;
+                            }
+                        }
+                    }
+
+                    WyswietlTablice("Tablica po sortowaniu:", lista);
+                    return;
                 }
                 else Console.WriteLine("Błąd! Liczba musi być z zakresu 3-20.");
             }
         }
 
-        private static int GetDigitsSum(int x)
+        private static void WyswietlTablice(string opis, List<int> lista, List<int> sumyCyfr)
+        {
+            int n = lista.Count;
+            Console.WriteLine("\n" + opis);
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.Write($"{lista[i]} (suma: {sumyCyfr[i]}), ");
+            }
+            Console.Write($"{lista[n - 1]} (suma: {sumyCyfr[n - 1]}) \n");
+        }
+
+        private static void WyswietlTablice(string opis, List<int> lista)
+        {
+            Console.WriteLine("\n" + opis);
+            foreach (var item in lista)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static int ObliczSumeCyfr(int x)
         {
             int sum = 0;
             while (x > 0)
@@ -50,16 +94,17 @@ namespace konsolowa1
             return sum;
         }
 
-        private static List<int> BubbleSort(List<int> list)
+        private static List<int> SortowanieBabelkowe(List<int> list)
         {
             int n = list.Count;
-            for (int i = n - 1; i >= 0; i--)
+            int temp;
+            for (int i = n - 1; i > 0; i--)
             {
                 for (int j = 0; j < i; j++)
                 {
                     if (list[j] > list[j + 1])
                     {
-                        int temp = list[j];
+                        temp = list[j];
                         list[j] = list[j + 1];
                         list[j + 1] = temp; 
                     }
