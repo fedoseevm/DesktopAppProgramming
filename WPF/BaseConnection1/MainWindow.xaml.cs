47be15c873336549f497e9be0b2d12798bb5f347
fedoseev <maksim.fedoseev122@gmail.com>
2026-01-22 14:26:30 +0100

    Add SQL base connection

---
+++ WPF/BaseConnection1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BaseConnection1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WczytajOsoby();
+        }
+
+        private void WczytajOsoby()
+        {
+            string connectionString =
+                @"Server=(localdb)\MSSQLLocalDB;Database=Respect;Trusted_Connection=True;";
+
+            string query = "SELECT * FROM osoby";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+            {
+                DataTable tabela = new DataTable();
+                adapter.Fill(tabela);
+                OsobyGrid.ItemsSource = tabela.DefaultView;
+            }
+        }
+    }
+}
