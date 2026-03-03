30d9e48e71b67cc74467a8e3018db024b4a59f99
fedoseevm <maksim.fedoseev122@gmail.com>
2026-03-03 10:36:04 +0100

    Add Projekt Opisywacz

---
+++ WPF/Opisywacz_ViewModel/OpisywaczViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opisywacz_ViewModel
+{
+    public partial class OpisywaczViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string autor = "Maksim";
+        [ObservableProperty]
+        private string cecha1;
+        [ObservableProperty]
+        private string cecha2;
+        [ObservableProperty]
+        private string cecha3;
+        [ObservableProperty]
+        private DateOnly data;
+
+        [RelayCommand]
+        private void WypiszCechy()
+        { 
+
+        }
+    }
+}
